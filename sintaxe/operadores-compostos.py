0f704e5c380c24f0650866b471fe8e47e76b3178
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-09-05 11:26:11 -0300

    Create operadores-compostos.py

---
+++ sintaxe/operadores-compostos.py
@@ -0,0 +1,24 @@
+
+#OPERADORES COMPOSTOS:
+
+x = 10
+print(f"x = {x}")
+
+x += 2
+print(f"x = {x}")
+
+x -= 4
+print(f"x = {x}")
+
+
+y = 5
+print(f"x = {y}")
+
+y *= 2
+print(f"y = {y}")
+
+y /= 2
+print(f"y = {y}")
+
+y %= 2
+print(f"y = {y}")
