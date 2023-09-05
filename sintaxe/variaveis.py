eacbf7e38a03c707a7ba4fff0f1e718f10448704
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-09-05 11:26:15 -0300

    Create variaveis.py

---
+++ sintaxe/variaveis.py
@@ -0,0 +1,13 @@
+#ATRIBUIÇÃO MULTIPLA
+a, b = 1, 2
+
+print("Antes da troca:")
+print(f"O valor das variáveis são: a={a} e b={b}")
+
+#PRIMEIRA TROCA
+c = a
+a = b
+b = c
+
+print("Primeira troca:")
+print(f"O valor das variáveis agora são: a={a} e b={b}")
