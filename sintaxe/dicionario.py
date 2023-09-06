7290991591ab87cef04f88f855d7c52b0160d97d
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-09-06 20:00:29 -0300

    Create dicionario.py

---
+++ sintaxe/dicionario.py
@@ -0,0 +1,6 @@
+pessoas = {111:['Marina'], 222:['Gabrielly'], 333:['Medeiros'], 444:['Souza']}
+
+print(pessoas)
+
+print(f'primeira pessoa: {pessoas[111]}')
+print(f'quarta pessoa: {pessoas[444]}')
