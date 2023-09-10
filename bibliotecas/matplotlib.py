acb57351e3d3b8637d89356ce9f000ad2e7783b9
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-09-09 23:54:07 -0300

    biblioteca matplotlib

---
+++ bibliotecas/matplotlib.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+# oferece uma ampla gama de recursos de visualização e é amplamente utilizada para criar gráficos
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([2, 5, 1, 10])
+
+plt.bar(x, y)
+plt.show()
